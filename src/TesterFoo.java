9ab26f269c0b1e687d56053b59c027305526ebd1
unknown <timaielsmith74@gmail.com>
2022-06-05 17:39:25 -0700

    first tryout commit

---
+++ src/TesterFoo.java
@@ -0,0 +1,10 @@
+
+
+
+public class TesterFoo {
+    public static void main(String[] args){ //shortcut type 'main' + tab
+      System.out.println("blah"); //sout + tab is a short cut too
+
+
+    }
+}
